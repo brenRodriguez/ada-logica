55e52adca2fb2be7df26aea04fda561b02be3711
Brenda Rodriguez <brendaerodriguez@hotmail.com>
2017-10-10 17:11:25 -0300

    ejercicios de logica en js

---
+++ logica/ejemplo.js
@@ -0,0 +1,128 @@
+/*var banda;
+alert('hola');
+confirm('chau');
+
+var numero= parseInt(prompt('Ingrese un numero'));
+alert(numero);
+
+//imprime la tabla del 9
+total=0;
+for(n=1;n<=10;n++){
+	total=n*9
+console.log(total);
+}
+
+var i=1;
+var total=0;
+do{
+	total=9*i;
+	i=i+1;
+}while(i<=10);
+
+/*
+
+//imprime la tabla del 9
+var i=0;
+var a=0;
+var n=10;
+
+while(i<=n){
+	a=i*9;
+	console.log(a);
+	i++;
+}*/
+/*
+var suma=0;
+var promedio=0;
+var n=50;
+if(n>0){
+	for(i=1; i<=50; i++){
+		suma=suma + i;
+	}
+	promedio=suma/n;
+	console.log(promedio);
+}
+
+suma+=numero;
+suma=suma+numero;
+
+//factorial de un numero	
+	var factorial=1;
+	var n=5
+	for(i=1; i<=n; i++){
+		factorial=factorial* i;
+	}
+	console.log(factorial);
+
+
+var semana= [50,35,105,40,25];
+for (i=0;i<semana.length;i++){
+	console.log(semana[i]);
+}
+
+
+
+//suma de todas las ventas de la semana
+var semana= [50,35,105,40,25];
+var suma=0
+//console.log(semana.length);
+for (i=0;i<semana.length;i++){
+	suma=suma+ semana[i];	
+	}
+console.log(suma);
+//prom= suma/semana.length;
+
+//mostrar la cantidad de dias que se vendio mas de 50.
+var semana= [50,35,105,40,25];
+for(i=0;i)
+//mostrar que dia de la semana habia comprado 50.
+*/
+var semana= [50,35,105,40,25];
+var dias=["lunes","martes","miercoles","jueves", "viernes"];
+var total=0;
+for(i=0;i<=semana.length;i++){
+	if(semana[i]>50){
+		console.log(dias[i]+":" + semana[i])
+	}
+}
+
+var notasMusicales =[];
+var nombre="Bren";
+var cantNombre= nombre.length;
+console.log("Bren tiene letras cant:" + cantNombre);
+
+notasMusicales[0]="Do";
+
+notasMusicales[1]="Re";
+
+notasMusicales[2]="Mi";
+
+notasMusicales[3]="Fa";
+
+notasMusicales[4]="Sol";
+
+var cant= notasMusicales.length;//5
+var ultimoElemento= notasMusicales.length-1
+console.log("Cant. notas:" + ultimoElemento);
+console.log("El ultimo elemento es:" + notasMusicales[notasMusicales.length-1])
+
+
+var frutas=[];
+frutas[0]="Pomelo";
+frutas[1]="Anana";
+frutas[2]="Frutilla";
+frutas[3]="Kiwi";
+frutas[4]="Sandia";
+var cant=frutas.length;
+var ultimaFruta= frutas.length-1;
+console.log("La ultima fruta de la lista es:"+ frutas[frutas.length-1] +" y la primer fruta de la lista es:"+ frutas[0]);//esto muestra el string correspondiente a la ultima posicion.
+console.log("La primer fruta de la lista es:"+ frutas[0]);
+
+
+//EJERCICIOS ITERADORES
+
+var elementos=[];
+var n=5;
+for(i=0;i<5;i++){
+	
+}
